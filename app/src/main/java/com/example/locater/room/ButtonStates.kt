b7eeb0765688ac96aad40bfc1da255f9b1f0a779
Ekta-14 <96908448+Ekta-14@users.noreply.github.com>
2024-02-22 11:15:31 +0530

    remove button state

--- app/src/main/java/com/example/locater/room/ButtonStates.kt
+++
@@ -1,8 +0,0 @@
-package com.example.locater.room
-
-import androidx.room.ColumnInfo
-
-data class ButtonStates(
-    @ColumnInfo(name = "btn_check_in_state") val checkInState: Boolean,
-    @ColumnInfo(name = "btn_check_out_state") val checkOutState: Boolean
-)
